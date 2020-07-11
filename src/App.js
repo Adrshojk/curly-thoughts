3597769f3b9535907f6474d22fe148931ef34045
adrshojk <adrshojk@gmail.com>
2020-07-11 15:42:04 +0530

    checking user login status

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import Routes from "routes";
 import { BrowserRouter as Router } from "react-router-dom";
 import TopBar from "components/topBar";
 import { CurrentUserProvider } from "context/currentUser";
-
+import CurrentUserChecker from 'components/currentUserChecker'
 const App = () => {
   return (
     <CurrentUserProvider>
+      <CurrentUserChecker>
       <div className="App">
         <Router>
           <TopBar />
           <Routes />
         </Router>
       </div>
+      </CurrentUserChecker>
     </CurrentUserProvider>
   );
 };
